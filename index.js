f528ed189761048f52733c7e62c7378799049f04
Arnab Kapri <77086712+A-Arnab-Z@users.noreply.github.com>
2026-01-08 13:21:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,9 +16,16 @@ app.get('/', (req, res) => {
 app.post('/generate-fir-pdf', async (req, res) => {
   try {
     const browser = await puppeteer.launch({
-      headless: 'new',
-      args: ['--no-sandbox', '--disable-setuid-sandbox'],
-    });
+  executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || '/usr/bin/chromium',
+  headless: 'new',
+  args: [
+    '--no-sandbox',
+    '--disable-setuid-sandbox',
+    '--disable-dev-shm-usage',
+    '--disable-gpu'
+  ],
+});
+
 
     const page = await browser.newPage();
 
